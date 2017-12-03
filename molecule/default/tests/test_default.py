c7aab97452415c7d97aaa8b774a0f41f5f1c5ebc
Stanislav Popov <popstas@gmail.com>
2017-12-03 06:50:09 +0500

    test: testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,16 +1,17 @@
+import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_authorized_keys_added(File):
-    f = File('/root/.ssh/authorized_keys')
+def test_authorized_keys_added(host):
+    f = host.file('/root/.ssh/authorized_keys')
     assert f.contains('test1@example.com')
     assert f.contains('test2@example.com')
 
 
-def test_authorized_keys_removed(File):
-    f = File('/root/.ssh/authorized_keys')
+def test_authorized_keys_removed(host):
+    f = host.file('/root/.ssh/authorized_keys')
     assert not f.contains('test3@example.com')
     assert not f.contains('test4@example.com')
